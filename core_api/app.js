09675f8a14b178b467183e3e0ac76649cffcd4f5
Fiqqar <145265758+Fiqqar@users.noreply.github.com>
2024-02-16 20:19:30 +0700

    16-02-24 (finish api)

--- core_api/app.js
+++ core_api/app.js
@@ -8,26 +8,29 @@ app.use(express.json());
 app.use(express.urlencoded({ extended : true }));
 app.use(cors(corsoptions));
 const PORT = process.env.PORT || 8080;
+require("./app/routes/user.routes")(app);
 app.listen(PORT, function() {
     console.log(`listening on port:${PORT}`); //
 })
-app.get('/user',(req, res) => {
-    const user = {
-        "name" : "example",
-        "id" : 1740,
-        "email" : "example@example.com"
-    };
-    res.send(user);
-});
+// app.get('/user',(req, res) => {
+//     const user = {
+//         "name" : "example",
+//         "id" : 1740,
+//         "email" : "example@example.com"
+//     };
+//     res.send(user);
+// });
 
-app.get('/',(req, res) => {
-    res.json({
-        message: "Welcome to my website..."
-    });
-});
+// app.get('/',(req, res) => {
+//     res.json({
+//         message: "Welcome to my website..."
+//     });
+// });
 
 const db = require("./app/models");
-db.sequelize.sync().then(() => {
+db.sequelize2.sync({
+    force: true,
+}).then(() => {
     console.log("sync successful");
 }).catch((err) => {
     console.log(err.message);
