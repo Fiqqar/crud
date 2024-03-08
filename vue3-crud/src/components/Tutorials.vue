73e547ca38f83161a2b612397bd86fc94ed54684
Fiqqar <145265758+Fiqqar@users.noreply.github.com>
2024-03-08 20:25:50 +0700

    8-3-24 (component tutorials)
    done
    -updating Tutorials.vue

--- vue3-crud/src/components/Tutorials.vue
+++ vue3-crud/src/components/Tutorials.vue
@@ -26,9 +26,79 @@
         </li>
       </ul>
 
-      <button class="m-3 btn btn-sm btn-danger" @click="removeAllTutorials">
+      <!-- <button class="m-3 btn btn-sm btn-danger" @click="removeAllTutorials">
         Remove All
-      </button>
+      </button> -->
+    </div>
+    <div class="col-md-6">
+      <div v-if="currentTutorial">
+        <h4>News</h4>
+        <div><label><strong>Title : </strong></label>{{ currentTutorial.title }}</div>
+        <div><label><strong>Description : </strong></label>{{ currentTutorial.description }}</div>
+        <div><label><strong>Status : </strong></label>{{ currentTutorial.published ?"published":"pending" }}</div>
+        <router-link to="'/tutorials/' + currentTutorial.id" class="badge badge-warning">
+          Edit
+        </router-link>
+      </div>
+      <div v-else>
+        <p>Please pick a news</p>
+      </div>
     </div>
 </div>
 </template>
+<script>
+import tutorialDataServices from "../services/TutorialDataServices";
+export default {
+  name : "Tutorials",
+  data() {
+    return {
+      tutorials : [], 
+      currentTutorial : null,
+      currentIndex : -1,
+      title : ""
+    };
+  },
+  methods : {
+    retrievetutorial() {
+      tutorialDataServices.getall().then(response => {
+        this.tutorials = response.data;
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    },
+    refreshlist() {
+      this.retrievetutorial();
+      this.currentTutorial = null;
+      this.currentIndex = -1; 
+    },
+    setActiveTutorial(tutorial, index) {
+      this.currentTutorial = tutorial;
+      this.currentIndex = index; 
+    },
+    // removeAllTutorials() {
+    //   tutorialDataServices.deleteall().then (response => {
+    //     console.log("All the news are deleted");
+    //     this.refreshlist();
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //   });
+    // },
+    searchTitle() {
+      tutorialDataServices.findbytitle(this.title).then (response => {
+        this.tutorials = response.data;
+        this.refreshlist();
+        console.log(response.data);
+      }) 
+      .catch(error => {
+        console.log(error);
+      });
+    }
+  },
+  mounted() {
+    this.retrievetutorial();
+  },
+};
+</script>
